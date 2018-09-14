a52b6b71a67d0b59226fd478a13be8227db67bb4
Yin Guo <554643726@qq.com>
2018-09-13 21:24:13 -0400

    HW1 initial commit

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,11 +5,23 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats lm
+# @importFrom stats lm
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+  # lm(formula, data)
+  X <- model.matrix(formula, data)
+  y <- data[, all.vars(formula)[1]]
+  svd_output <- svd(X)
+  U <- svd_output[["u"]]
+  Sinv <- diag(1 / svd_output[["d"]])
+  V <- svd_output[["v"]]
+  pseudo_inv <- V %*% Sinv %*% t(U)
+  betahat <- pseudo_inv %*% y
+  flm <- list()
+  flm$coefficients <- betahat
+  class(flm) <- "lm"
+  return(flm)
 }
