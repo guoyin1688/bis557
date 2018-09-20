e692d8a5fb9c4530cab47014deb424762993cbbb
Guo <yg336@STC13-0163.student.yale.internal>
2018-09-19 22:49:57 -0400

    HW1 replace svd with qr

--- R/linear-models.r
+++ R/linear-models.r
@@ -14,20 +14,22 @@ linear_model <- function(formula, data) {
   # lm(formula, data)
   X <- model.matrix(formula, data)
   y <- data[, all.vars(formula)[1]]
-  svd_output <- svd(X)
-  U <- svd_output[["u"]]
-  Sinv <- diag(1 / svd_output[["d"]])
-  V <- svd_output[["v"]]
-  pseudo_inv <- V %*% Sinv %*% t(U)
-  betahat <- pseudo_inv %*% y
+  betahat <- qr.solve(X, y)
+  betahat[which(betahat == 0)] <- NA
+  # svd_output <- svd(X)
+  # U <- svd_output[["u"]]
+  # Sinv <- diag(1 / svd_output[["d"]])
+  # V <- svd_output[["v"]]
+  # pseudo_inv <- V %*% Sinv %*% t(U)
+  # betahat <- pseudo_inv %*% y
   y_fit = X %*% betahat
   residuals = y - y_fit
   X_qr = qr(X)
   flm <- list(coefficients = betahat, residals = residuals, fitted.values = y_fit,
               rank = ncol(X), weights = NULL, df.residual = nrow(X) - ncol(X), 
               call = call('lm', formula), terms = terms(x = formula, data = data),
-              contrasts = NA, xlevels = NA, offset = NA, y = y, x = X, 
-              model = formula, na.action = NA, qr = X_qr)
+              contrasts = NULL, xlevels = NULL, offset = NULL, y = y, x = X, 
+              model = formula, na.action = NULL, qr = X_qr)
   class(flm) <- "lm"
   return(flm)
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ BIS557
 
 This is a repository for storing all code, documentation, and digital artifacts for BIS557.
 
-I've implemented and documented a function that calls `linear_model`. You can use it like this:
+I've implemented and documented a function that realizes linear_model. You can use it like this:
 
 ```{R}
 library(bis557)
