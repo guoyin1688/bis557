78637209b3853cb8b09f0945cc9092e30372a5d6
guoyin1688 <554643726@qq.com>
2018-12-19 23:46:40 +0800

    HW5

--- data-raw/make-df.r
+++ data-raw/make-df.r
@@ -3,8 +3,11 @@ dir.create("../data")
 lm_patho <- read.csv("df.csv")
 ridge_train <- read.csv("ridge_train.csv")
 ridge_test <- read.csv("ridge_test.csv")
-
+emnist_train <- read.csv("emnist-letters-train-sample.csv")
+emnist_test <- read.csv("emnist-letters-test-sample.csv")
 
 save(lm_patho, file = "../data/lm_patho.rda")
 save(ridge_train, file = "../data/ridge_train.rda")
 save(ridge_test, file = "../data/ridge_test.rda")
+save(emnist_train, file = "../data/emnist_train.rda")
+save(emnist_test, file = "../data/emnist_test.rda")
